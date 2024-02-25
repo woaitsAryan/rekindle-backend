dd3384fe7e0b41d1d090bdc2ab82b17d55eca138
woaitsAryan <yoprince.ab@gmail.com>
2024-02-26 00:22:04 +0530

    feat: changed gpt to gemma

--- helpers/llm.py
+++ helpers/llm.py
@@ -1,24 +1,33 @@
-from openai import OpenAI
-import os
-from dotenv import load_dotenv
+# from openai import OpenAI
+# import os
+# from dotenv import load_dotenv
+import ollama
 
-load_dotenv()
+# load_dotenv()
 
 def get_llm_response(emotion: str) -> str:
-    client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),
-    )
+    # client = OpenAI(
+    # api_key=os.getenv("OPENAI_API_KEY"),
+    # )
 
-    gpt_prompt = f"I want you to be very emotionally recpeptive and act as an empathic companion to me. I am going to detail the events and experiences of today as a story. Please give me advice and encouragement on that. My day was: {emotion}"
+    gpt_prompt = f"I am going to give you a story of the events I experienced today. Please try to be empathetic, compassionate, and provide advice, encouragement, and a have a chat with me on the same within 150 words. This is my story: {emotion}"
     
-    chat_completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "user",
-                "content": gpt_prompt,
-            },
-        ],
-    )
+    # chat_completion = client.chat.completions.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {
+    #             "role": "user",
+    #             "content": gpt_prompt,
+    #         },
+    #     ],
+    # )
+    
+    responsellm = ollama.chat(model='gemma:2b', messages=[
+        {
+            'role': 'user',
+            'content': gpt_prompt
+        }
+    ])
 
-    return chat_completion.choices[0].message.content # type: ignore
+    # return chat_completion.choices[0].message.content # type: ignore
+    return responsellm['message']['content']
